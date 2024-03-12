93b9c031e7d47ba6ce857523ce042f83c7984be1
DariaSuhan2 <157480602+DariaSuhan2@users.noreply.github.com>
2024-03-12 11:56:49 +0200

    Add files via upload

---
+++ InMemoryDataAccess/CarCategoryRepository.cs
@@ -0,0 +1,57 @@
+﻿using DomainDefinition;
+using DomainDefinition.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InMemoryDataAccess
+{
+    public class CarCategoryRepository : ICarCategoryRepository
+    {
+        private List<CarCategory> allCategories = new List<CarCategory>();
+
+        public CarCategoryRepository()
+        {
+
+        }
+
+        public List<CarCategory> GetAll()
+        {
+            return this.allCategories;
+        }
+
+        public CarCategory? GetOne(string name)
+        {
+            var found = allCategories.Where(x => x.Name == name).FirstOrDefault();
+            return found;
+        }
+      
+        public void DeleteAll()
+        {
+            allCategories = new List<CarCategory>();
+        }
+
+        public void DeleteOne(string name)
+        {
+            var found = GetOne(name);
+            if (found != null)
+            {
+                allCategories.Remove(found);
+            }
+        }
+
+        public CarCategory Insert(string name, int engineCapacity, int weight)
+        {
+            var found = GetOne(name);
+            if (found == null)
+            {
+                var newCategory = new CarCategory(name, engineCapacity, weight);
+                allCategories.Add(newCategory);
+                return newCategory;
+            }
+            else return found;
+        }
+    }
+}
---
+++ InMemoryDataAccess/CarRepository.cs
@@ -0,0 +1,88 @@
+﻿using DomainDefinition;
+using DomainDefinition.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InMemoryDataAccess
+{
+    public class CarRepository : ICarRepository 
+    {
+
+        private  List<Car> allCars = new List<Car>();
+
+
+        public CarRepository()
+        {
+
+        }
+
+        public List<Car> GetAll()
+        {
+            return this.allCars;
+        }
+
+        public Car? GetOne(int vin)
+        {
+            var found = allCars.Where(x => x.vin == vin).FirstOrDefault();
+            return found;
+        }
+
+        public List<Car> GetCarsWithAirConditioning(bool airConditioning)
+        {
+            return allCars.Where(x => x.AirConditioning == true).ToList<Car>();
+        }     
+        
+        public void DeleteAll()
+        {
+            allCars = new List<Car>();
+        }
+        public void DeleteOne(int vin)
+        {
+            var found = GetOne(vin);
+            if (found != null)
+            {
+               allCars.Remove(found);
+            }
+        }
+
+        public Car Insert(Car carInstance)
+        {
+            var found = GetOne(carInstance.vin);
+            if (found == null)
+            {
+                allCars.Add(carInstance);
+                return carInstance;
+            }
+            else return found;
+        }
+
+        public Car UpdateCar(Car carInstance)
+        {
+            var foundCar = GetOne(carInstance.vin);
+            if (foundCar != null)
+            {
+                foundCar.Color = carInstance.Color;
+            }
+            return foundCar;
+        }
+
+        /*public Car UpdateColor(int vin, string color)
+        {
+            var foundCar = GetOne(vin);
+            if (foundCar != null)
+            {
+                foundCar.Color = color;
+            }
+            return foundCar;
+        }*/
+
+        public int GetCategoryCount(string name)
+        {
+
+          return allCars.Count;
+        }
+    }
+}
