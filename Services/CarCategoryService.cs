84b83fc811728599ada929fc327954ea045c37cb
DariaSuhan2 <157480602+DariaSuhan2@users.noreply.github.com>
2024-03-12 11:57:44 +0200

    Add files via upload

---
+++ Services/CarCategoryService.cs
@@ -0,0 +1,66 @@
+﻿using DomainDefinition;
+using DomainDefinition.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+//using TextFileDataAccess;
+using SQLDataAccess;
+using static Azure.Core.HttpHeader;
+
+namespace Services
+{
+    public class CarCategoryService
+    {
+        private ICarCategoryRepository repository;
+        public ICarRepository carRepository;
+
+        public CarCategoryService()
+        {
+            repository = new CarCategoryRepository();
+            carRepository = new CarRepository();
+        }
+
+        public List<CarCategory> GetAllCategories()
+        {
+            return this.repository.GetAll();
+        }
+
+        public CarCategory? GetCategory(string name)
+        {
+            return this.repository.GetOne(name);
+        }
+
+        public CarCategory AddCategory(string name, int engineCapacity, int weight)
+        {
+            return this.repository.Insert(name, engineCapacity, weight);
+        }
+
+        public List<string> DeleteAllCategories()
+        {
+            var result = new List<string>();
+            foreach (var category in this.repository.GetAll())
+            {
+                result.Add(DeleteCategory(category.Name));
+            }
+            return result;
+        }
+
+        public string DeleteCategory(string name)
+        {
+            var a = this.carRepository.GetCategoryCount(name);
+            if (a == 0)
+            {
+                this.repository.DeleteOne(name);
+                return $"Categoria {name} a fost stearsa";
+            }
+            else
+            {
+                return $"Categoria {name} nu poate fi stearsa pentru ca este folosita ca si categorie pentru o masina";
+            }
+        }
+    }
+}
---
+++ Services/CarService.cs
@@ -0,0 +1,80 @@
+﻿using DomainDefinition;
+using DomainDefinition.Contracts;
+//using TextFileDataAccess;
+using SQLDataAccess;
+
+namespace Services
+{
+    public class CarService
+    {
+        // public static List<Car> _allCars = new List<Car>();
+
+        public ICarRepository carRepository;
+
+        public CarService()
+        {
+            carRepository = new CarRepository();
+        }
+
+        public List<Car> GetAllCars()
+        {
+            return this.carRepository.GetAll();
+        }
+
+
+        public Car? GetCar(int vin)
+        {
+            return this.carRepository.GetOne(vin);
+        }
+
+        public Car AddCar(Car carInstance)
+        {
+            return this.carRepository.Insert(carInstance);
+        }
+
+        public void AddBudgetCar(int vin, string color, string brand, int doorNr, CarCategory carCategory, Enums.Fuel fuel)
+        {
+            var carInstance = new BudgetCar(vin, color, brand, doorNr, carCategory, fuel);
+            AddCar(carInstance);
+        }
+
+        public void AddPremiumCar(int vin, string color, string brand, int doorNr, CarCategory carCategory, Enums.Fuel fuel)
+        {
+            var carInstance = new PremiumCar(vin, color, brand, doorNr, carCategory, fuel);
+            AddCar(carInstance);
+        }
+
+        public void AddLuxuryCar(int vin, string color, string brand, int doorNr, CarCategory carCategory, Enums.Fuel fuel)
+        {
+            var carInstance = new LuxuryCar(vin, color, brand, doorNr, carCategory, fuel);
+            AddCar(carInstance);
+        }
+
+        /*public void ChangeColor(int vin, string color)
+        {
+            this.carRepository.UpdateColor(carInstance);
+        } */
+        public void ChangeCar(Car carInstance)
+        {
+            this.carRepository.UpdateCar(carInstance);
+        }
+
+        public List<Car> GetCarsWithAirConditioning(bool airConditioning)
+        {
+            return this.carRepository.GetCarsWithAirConditioning(airConditioning);
+        }
+        public void DeleteAllCars()
+        {
+            this.carRepository.DeleteAll();
+        }
+
+        public void DeleteCar(int vin)
+        {
+
+            this.carRepository.DeleteOne(vin);
+        }
+
+
+
+    }
+}
