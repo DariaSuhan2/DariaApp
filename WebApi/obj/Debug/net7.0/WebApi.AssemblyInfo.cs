99fb9e0b92faead3d003acf6f00497e3c4218b31
DariaSuhan2 <lupsa.daria@gmail.com>
2024-03-19 10:28:24 +0200

    <feat> added the CreatedOn si UpdatedOn in the Car class

--- WebApi/obj/Debug/net7.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net7.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31b092fbac866e1f8322237dbf76add9dc7560c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47cfe6a9c05a6f913a3ec37bab9faa8dca6fe99a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
